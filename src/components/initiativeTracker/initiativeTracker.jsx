9518929e0d748439eba66bfacc006410912a6b2b
Jarrod <jard-shoe@hotmail.com>
2023-03-04 09:50:30 +1300

    sort the initiative list

--- src/components/initiativeTracker/initiativeTracker.jsx
+++ src/components/initiativeTracker/initiativeTracker.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import Character from "./character";
 
 function InitiativeTracker(props) {
-  let list = props.characters;
-  if (list.length === 0)
+  let list = props.characters.slice();
+  if (list.length === 0) {
     return (
       <React.Fragment>
         <h1>Initiative Tracker</h1>
         <h2>There are no Characters, add some Heroes above or Enemies below</h2>
       </React.Fragment>
     );
+  }
 
+  //sort the list based on initiative value, before rendering it.
+  const sortedList = sortList(list);
   return (
     <React.Fragment>
       <h1>Initiative Tracker</h1>
-      {props.characters.map((x) => (
+      {sortedList.map((x) => (
         <Character
           key={x.id}
           id={x.id}
@@ -29,3 +32,16 @@ function InitiativeTracker(props) {
 }
 
 export default InitiativeTracker;
+
+function sortList(list) {
+  //sort the list
+  let sortedList = [];
+  for (let init = 30; init >= 0; init--) {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].initValue === init) {
+        sortedList = sortedList.concat(list[i]);
+      }
+    }
+  }
+  return sortedList;
+}
